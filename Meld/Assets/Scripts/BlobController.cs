0099b3630bb2ee9ecc23a50fb7093c6505cf056a
unknown <yuantao.gu@mail.utoronto.ca>
2019-01-19 15:38:11 -0500

    Changed sphere colliders to capsule colliders

---
+++ Meld/Assets/Scripts/BlobController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobController : MonoBehaviour
+{
+
+    Cloth cloth;
+    SphereCollider sphere;
+    public GameObject player1;
+    public GameObject player2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cloth = GetComponent<Cloth>();
+        sphere = GetComponent<SphereCollider>();
+        // player1 = cloth.sphereColliders[0].first;
+        // player2 = cloth.sphereColliders[1].first;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var position1 = player1.transform.position;
+        var position2 = player2.transform.position;
+        var midpoint = (position1 + position2) / 2;
+
+        transform.position = midpoint;
+        // Updating the position of the cloth to prevent it from breaking off
+        if (Vector3.Distance(transform.position, midpoint) < 3)
+        {
+            cloth.ClearTransformMotion();
+            cloth.transform.position = midpoint;
+        }
+        
+        // Setting the 
+        // var dir = position1 - position2;
+        // transform.rotation = Quaternion.FromToRotation(Vector3.up, dir);
+    }
+}
