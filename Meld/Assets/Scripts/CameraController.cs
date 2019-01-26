5504c52af87fc3b2c7882e06c106121abb957a63
unknown <yuantao.gu@mail.utoronto.ca>
2019-01-25 23:28:10 -0500

    Added Nvidia flex assets and created FlexTest scene for testing flex

---
+++ Meld/Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Copied code from Survival Shooter Tutorial
+    
+    public Transform player1;
+    public Transform player2;           
+    public float smoothing = 5f;        // The speed with which the camera will be following.
+
+    Vector3 offset;                     // The initial offset from the target.
+
+
+    void Start ()
+    {
+        // Calculate the initial offset.
+        offset = transform.position - getTargetPos();
+    }
+
+    void FixedUpdate ()
+    {
+        // Create a postion the camera is aiming for based on the offset from the target.
+        Vector3 targetCamPos = getTargetPos() + offset;
+
+        // Smoothly interpolate between the camera's current position and it's target position.
+        transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+
+    Vector3 getTargetPos (){
+        return (player1.position + player2.position) / 2;
+    }
+}
---
+++ Meld/Assets/Scripts/move3.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move3 : MonoBehaviour
+{
+    public float speed = 1;
+    public float topSpeed = 2;
+    private Rigidbody cube;
+    Vector3 movement;
+
+    public string inputHorizontal;
+    public string inputVertical;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube = GetComponent<Rigidbody>();
+        cube.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis (inputHorizontal) *20;
+        float vertical = Input.GetAxis (inputVertical)* 20;
+
+        Vector3 movement = new Vector3 (horizontal, 0.0f, vertical);
+
+        //cube.AddForce(movement * speed);
+
+        
+        // Controlling movement speed on the XZ plane
+        movement = movement.normalized * speed;
+        if (movement.magnitude > topSpeed)
+            movement = movement.normalized * topSpeed;
+        // This is to preserve Y movement so that gravity affects it properly
+        movement.y = cube.velocity.y;
+        cube.velocity = movement;
+
+
+        //Vector3 vel = cube.velocity;
+        //vel.x = horizontal;
+        //vel.y = vertical;
+
+//        cube.velocity = vel;
+    }
+}
