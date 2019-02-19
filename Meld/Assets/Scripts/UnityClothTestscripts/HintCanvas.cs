3c9418fdba8884a8de1353d19885fb283f9c7a3c
Frankisflying <hyphyphy@163.com>
2019-02-19 14:25:08 -0500

    Added two scripts

---
+++ Meld/Assets/Scripts/UnityClothTestscripts/HintCanvas.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HintCanvas : MonoBehaviour
+{
+    public Canvas hintCanvas;   
+    
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        hintCanvas = this;
+        hintCanvas.enabled = false;
+    }
+
+    public void DisplayHint()
+    {
+        hintCanvas.enabled = true;
+    }
+
+    public void HideHint()
+    {
+        hintCanvas.enabled = false;
+    }
+}
---
+++ Meld/Assets/Scripts/UnityClothTestscripts/TensionSlider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TensionSlider : MonoBehaviour
+{
+    public Slider TensionSlider;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        TensionSlider = this;
+    }
+
+    public void SetTension(float playerDistance)
+    {
+        TensionSlider.value = playerDistance;
+    }
+}
