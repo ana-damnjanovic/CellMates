38486106f1e39eea8db752e973535c768f8895ec
Frankisflying <hyphyphy@163.com>
2019-02-22 21:30:35 -0500

    Initial Checkpoint script

---
+++ Meld/Assets/Scripts/current-scripts/Checkpoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private CheckpointManager cm;
+    void Start()
+    {
+        cm = GameObject.FindGameObjectWithTag("CM").GetComponent<CheckpointManager>();
+    }
+    // called whenever another collider enters our zone (if layers match)
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Players"))
+        {
+            cm.lastCheckpointPosition = transform.position;
+        }
+    }
+}
