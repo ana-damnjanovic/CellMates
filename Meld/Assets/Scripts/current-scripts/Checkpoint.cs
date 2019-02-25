f3f9a9a3beddfcb0ace2ebcbf44239369b27bac0
Frankisflying <hyphyphy@163.com>
2019-02-25 00:33:03 -0500

    Fixed cloth and did some alteration with checkpoint script

--- Meld/Assets/Scripts/current-scripts/Checkpoint.cs
+++ Meld/Assets/Scripts/current-scripts/Checkpoint.cs
@@ -26,18 +26,24 @@ public class Checkpoint : MonoBehaviour
             if (player1Grounded)
             {
                 player1GroundedHit = player1behaviour.GetGroundedHit();
-                if (player1GroundedHit.transform.gameObject == gameObject)
+                if (player1GroundedHit.transform.gameObject == gameObject && checkpointset == false)
                 {
-                    cm.lastCheckpointPosition = GameObject.FindGameObjectWithTag("Player1").transform.position;
+                    cm.lastCheckpointPositionp1 = GameObject.FindGameObjectWithTag("Player1").transform.position;
+                    cm.lastCheckpointPositionp2 = GameObject.FindGameObjectWithTag("Player2").transform.position;
+                    cm.lastCheckpointPositionmem = GameObject.FindGameObjectWithTag("Membrane").transform.position;
+                    cm.lastCheckpointPositionmemsphere = GameObject.FindGameObjectWithTag("MembraneSupportSphere").transform.position;
                     checkpointset = true;
                 }
             }
             if (player2Grounded)
             {
                 player2GroundedHit = player2behaviour.GetGroundedHit();
-                if (player2GroundedHit.transform.gameObject == gameObject)
+                if (player2GroundedHit.transform.gameObject == gameObject && checkpointset == false)
                 {
-                    cm.lastCheckpointPosition = GameObject.FindGameObjectWithTag("Player2").transform.position;
+                    cm.lastCheckpointPositionp1 = GameObject.FindGameObjectWithTag("Player1").transform.position;
+                    cm.lastCheckpointPositionp2 = GameObject.FindGameObjectWithTag("Player2").transform.position;
+                    cm.lastCheckpointPositionmem = GameObject.FindGameObjectWithTag("Membrane").transform.position;
+                    cm.lastCheckpointPositionmemsphere = GameObject.FindGameObjectWithTag("MembraneSupportSphere").transform.position;
                     checkpointset = true;
                 }
             }
