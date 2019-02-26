ad7a0437095fe5785405b08da1148f424aef7aaf
ana-damnjanovic <ana.damnjanovic@mail.utoronto.ca>
2019-02-26 00:29:11 -0500

    moved velocity logic to playerBehaviour and created GameManager with global variables

---
+++ Meld/Assets/Scripts/current-scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    public static float maxSeparation = 2.85f;
+    public static float speed = 1;
+    public static float topSpeed = 3;
+    public static float rayCastDistance = 0.75f;
+
+    void Awake()
+    {
+        if (Instance == null) { Instance = this; } else if (Instance != this) { Destroy(this); }
+        DontDestroyOnLoad(gameObject);
+    }
+}
--- Meld/Assets/Scripts/current-scripts/playerBehaviour.cs
+++ Meld/Assets/Scripts/current-scripts/playerBehaviour.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 
 public class playerBehaviour : MonoBehaviour
 {
+    private float speed = GameManager.speed;
+    private float topSpeed = GameManager.topSpeed;
     private bool isGrounded;
     private bool isSticking = false;
     private RaycastHit playerGroundedHit;
     private GameObject player;
     private Rigidbody rb;
-    public float raycastDistance = 0.75f;
+    private float raycastDistance = GameManager.rayCastDistance;
 
     public RaycastHit GetGroundedHit()
     {
@@ -35,6 +37,22 @@ public class playerBehaviour : MonoBehaviour
     public void SetIsSticking(bool isPlayerSticking)
     {
         isSticking = isPlayerSticking;
+    }
+
+    public void SetVelocity(float horizontalAxis, float verticalAxis)
+    {
+        float horizontal = horizontalAxis * 20;
+        float vertical = verticalAxis * 20;
+
+        Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
+ 
+        movement = movement.normalized * speed;
+        if (movement.magnitude > topSpeed)
+            movement = movement.normalized * topSpeed;
+        // This is to preserve Y movement so that gravity affects it properly
+        movement.y = rb.velocity.y;
+
+        rb.velocity = movement;
     }
 
     // Start is called before the first frame update
