d8c419974ee8698bd70b485b5672a697ca5f3c3d
ana-damnjanovic <ana.damnjanovic@mail.utoronto.ca>
2019-03-12 03:26:09 -0400

    added StickableAxis

---
+++ Meld/Assets/Scripts/current-scripts/StickableAxis.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickableAxis : MonoBehaviour
+{
+    // the axis we want to boost in the move script
+    public bool X;
+    public bool Y;
+    public bool Z;
+
+    public Vector3 GetStickableAxis()
+    {
+        if (X) return Vector3.right;
+        if (Y) return Vector3.up;
+        if (Z) return Vector3.forward;
+        else return Vector3.zero;
+    }
+}
