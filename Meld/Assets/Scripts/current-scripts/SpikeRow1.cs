0149de42c439e193cbbf91e355bdd2e6ce49aeaa
Frankisflying <hyphyphy@163.com>
2019-03-25 18:12:22 -0400

    With Spikes!

---
+++ Meld/Assets/Scripts/current-scripts/SpikeRow1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeRow1 : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Go());
+    }
+
+    // Update is called once per frame
+    IEnumerator Go()
+    {
+        while (true) {
+            yield return new WaitForSeconds(3f);
+            anim.speed = 0;
+        }
+    }
+}
